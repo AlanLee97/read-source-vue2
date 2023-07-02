f5a0dad4be1f0bc312e0baef80288ff4cbad083f
AlanLee <1445654576@qq.com>
2023-07-02 10:46:37 +0800

    fix: 更新demo

--- examples/simple-demo/app.js
+++ examples/simple-demo/app.js
@@ -2,30 +2,82 @@
 /* eslint-disable no-debugger */
 
 debugger
+const logStyle = 'color: blue; font-weight: bold;'
 
-Vue.component('count-demo', {
+const countDemo = {
   name: 'CountDemo',
   template: `
     <section class="cpn--count-demo">
       <div>count: {{count}}</div>
       <div><button @click="add">+ 1</button></div>
+      <div class="slot-content">
+        <slot></slot>
+      </div>
     </section>
   `,
+  props: {
+    initValue: {
+      type: Number,
+      default: 0
+    }
+  },
   data() {
     return {
       count: 0
     }
   },
+  computed: {
+    countComputed() {
+      return this.initValue
+    }
+  },
+  beforeCreate() {
+    console.log('%c count-demo->lifecycle beforeCreate', logStyle)
+  },
+  created() {
+    console.log('%c count-demo->lifecycle created', logStyle)
+
+    this.count = this.initValue
+  },
+  beforeMount() {
+    console.log('%c count-demo->lifecycle beforeMount', logStyle)
+  },
+  mounted() {
+    console.log('%c count-demo->lifecycle mounted', logStyle)
+    console.log('countComputed', this.countComputed);
+  },
+  beforeUpdate() {
+    console.log('%c count-demo->lifecycle beforeUpdate', logStyle)
+  },
+  updated() {
+    console.log('%c count-demo->lifecycle updated', logStyle)
+  },
+  beforeDestroy() {
+    console.log('%c count-demo->lifecycle beforeDestroy', logStyle)
+  },
+  destroyed() {
+    console.log('%c count-demo->lifecycle destroyed', logStyle)
+  },
+
+  watch: {
+    count: function (newVal) {
+      console.log('count-demo-> watch count', newVal)
+    }
+  },
+
   methods: {
     add() {
       this.count = this.count + 1
     }
   }
-})
+}
 
 // app Vue instance
 var app = new Vue({
   name: 'SimpleDemo',
+  components: {
+    countDemo
+  },
   data() {
     return {
       msg: 'Hello Simple Demo'
